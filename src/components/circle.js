76daaee73bfd6fbcae6694a0434885fd81b67c07
Dharmendra Singh Yadav <87720840+dharmendra022@users.noreply.github.com>
2021-08-27 00:09:51 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,13 +1,6 @@
-// import using require
-
 var Shape = require('./shape.js');
 class Circle extends Shape {
     constructor() { super(); this.radius = 9; }
     calculateArea() { return "Calculated area of circle"; }
 }
 module.exports = Circle;
-
-// declare class
-
-
-// export class using module.exports
